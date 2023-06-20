146f33e946212d2fe5cd2d787de545e49998b89e
pnocera <pnocera@gmail.com>
2023-06-20 10:26:11 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { App, Tldraw } from '@tldraw/tldraw';
+
+import type { ComponentProps } from 'react';
+import { Utils } from './utils/urls';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'tldraw-angular-example';
+export class AppComponent implements OnDestroy {
+  Tldraw = Tldraw;
+  props!: ComponentProps<typeof Tldraw>;
+  app!: App;
+  darkmode = false;
+
+  constructor() {
+    const assetsurl = Utils.getAssetUrlsByMetaUrl({
+      baseUrl: 'http://localhost:4200',
+    });
+
+    this.props = {
+      assetUrls: assetsurl,
+
+      onMount: this.handleMount.bind(this),
+    };
+  }
+  ngOnDestroy(): void {
+    this.app?.dispose();
+  }
+
+  toggleDarkMode() {
+    this.darkmode = !this.darkmode;
+    this.app.setDarkMode(this.darkmode);
+  }
+
+  handleMount(app: App) {
+    this.app = app;
+    app.setGridMode(true);
+
+    app.updateInstanceState({
+      isDebugMode: false,
+    });
+
+    app.updateUserDocumentSettings({
+      isDarkMode: this.darkmode,
+      isSnapMode: true,
+    });
+  }
 }
